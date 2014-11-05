ac8fd33cebb309b1f77cef6946b166d1b6b08c78
noname55804 <marlin2201@gmail.com>
2014-11-05 17:46:31 +0100

    Fix moving animation settings

--- src/com/android/settings/hacker/AnimationSettings.java
+++ src/com/android/settings/hacker/AnimationSettings.java
@@ -43,8 +43,19 @@ import com.android.settings.Utils;
 import com.android.settings.liquid.util.Helpers;
 
 public class AnimationSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
+		
+	private static final String KEY_ANIMATION_OPTIONS = "category_animation_options";
+    private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
+    private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
+    private static final String KEY_POWER_CRT_MODE = "system_power_crt_mode";	
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
 
+    private ListPreference mCrtMode;
+    private ListPreference mListViewAnimation;
+    private ListPreference mListViewInterpolator;
+	private ListPreference mToastAnimation;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +64,47 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
+		// respect device default configuration
+        // true fades while false animates
+        boolean electronBeamFadesConfig = getResources().getBoolean(
+                com.android.internal.R.bool.config_animateScreenLights);
+        PreferenceCategory animationOptions =
+            (PreferenceCategory) prefSet.findPreference(KEY_ANIMATION_OPTIONS);
+        mCrtMode = (ListPreference) prefSet.findPreference(KEY_POWER_CRT_MODE);
+        if (mCrtMode != null) {
+            if (!electronBeamFadesConfig) {
+                int crtMode = Settings.System.getInt(getContentResolver(),
+                        Settings.System.SYSTEM_POWER_CRT_MODE, 1);
+                mCrtMode.setValue(String.valueOf(crtMode));
+                mCrtMode.setSummary(mCrtMode.getEntry());
+                mCrtMode.setOnPreferenceChangeListener(this);
+            } else if (animationOptions != null) {
+                animationOptions.removePreference(mCrtMode);
+            }
+        }
+		
+		mListViewAnimation = (ListPreference) prefSet.findPreference(KEY_LISTVIEW_ANIMATION);
+        int listviewanimation = Settings.System.getInt(getContentResolver(),
+                Settings.System.LISTVIEW_ANIMATION, 0);
+        mListViewAnimation.setValue(String.valueOf(listviewanimation));
+        mListViewAnimation.setSummary(mListViewAnimation.getEntry());
+        mListViewAnimation.setOnPreferenceChangeListener(this);
+
+        mListViewInterpolator = (ListPreference) prefSet.findPreference(KEY_LISTVIEW_INTERPOLATOR);
+        int listviewinterpolator = Settings.System.getInt(getContentResolver(),
+                Settings.System.LISTVIEW_INTERPOLATOR, 0);
+        mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
+        mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
+        mListViewInterpolator.setOnPreferenceChangeListener(this);
+        mListViewInterpolator.setEnabled(listviewanimation > 0);
+		
+		mToastAnimation = (ListPreference) prefSet.findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(
+                getContentResolver(),Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation);
+        mToastAnimation.setOnPreferenceChangeListener(this);
+		
         UpdateSettings();
     }
 
@@ -71,13 +123,46 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
     }
 	
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        //if (preference == mDummy) {
-        //    int value = Integer.parseInt((String) newValue);
-        //    Settings.System.putInt(getContentResolver(),
-        //            Settings.System.RECENT_PANEL_SCALE_FACTOR, value);
-        //    return true;
-        //}
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+		final String key = preference.getKey();
+		if (KEY_POWER_CRT_MODE.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mCrtMode.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.SYSTEM_POWER_CRT_MODE,
+                    value);
+            mCrtMode.setSummary(mCrtMode.getEntries()[index]);
+        }
+        if (KEY_LISTVIEW_ANIMATION.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mListViewAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LISTVIEW_ANIMATION,
+                    value);
+            mListViewAnimation.setSummary(mListViewAnimation.getEntries()[index]);
+            mListViewInterpolator.setEnabled(value > 0);
+        }
+        if (KEY_LISTVIEW_INTERPOLATOR.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            int index = mListViewInterpolator.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LISTVIEW_INTERPOLATOR,
+                    value);
+            mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
+        }
+		if (KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.WAKEUP_WHEN_PLUGGED_UNPLUGGED,
+                    (Boolean) objValue ? 1 : 0);
+        }
+        if (KEY_TOAST_ANIMATION.equals(key)) {
+            int index = mToastAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.TOAST_ANIMATION, (String) objValue);
+            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+            Toast.makeText(getActivity(), "Toast animation test!!!",
+                    Toast.LENGTH_SHORT).show();
+        }
         return true;
     }
 
